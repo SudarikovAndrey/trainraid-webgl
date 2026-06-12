6340d58f1847f2578c889fefdd27a3c92b6eef59
Andrey Sudarikov <andrey@rolltorule.com>
2026-06-12 12:00:00 +0000

    standalone TrainRaid: base files

---
+++ Build/parts.js
@@ -0,0 +1,2 @@
+var DATA_PARTS=["Build/webgl_train.data.unityweb.partaa","Build/webgl_train.data.unityweb.partab","Build/webgl_train.data.unityweb.partac","Build/webgl_train.data.unityweb.partad"];
+var WASM_PARTS=["Build/webgl_train.wasm.unityweb"];
